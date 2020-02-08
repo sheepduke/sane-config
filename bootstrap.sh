ea4503b4cd69c8b78617b4663503e3fea93a2c9b
YUE Daian <sheepduke@gmail.com>
2020-02-09 00:27:59 +0800

    feat: add more configuration files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-cd ~/
-
-# TODO
-# Design the usage of this script, especially the PWD.
-
 mkdir -p ~/projects/config/
 
-# Oh My ZSH.
-git clone git@github.com:/ohmyzsh/ohmyzsh .oh-my-zsh
-# TODO fix path problem.
-cp .zshrc ~/
-cp .tmux.conf ~/
+# Oh My ZSH
+echo "Setting up ZSH..."
+git clone git@github.com:/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+ln zshrc ~/.zshrc
+ln tmux.conf ~/.tmux.conf
 
-# StumpWM.
-git clone git@github.com:/sheepduke/stumpwm-settings ~/projects/config/stumpwm-settings
-git clone git@github.com:/stumpwm/stumpwm-contrib ~/.stumpwm.d/modules
-# TODO fix path problem.
-cp .xinitrc .Xresource ~/
+# Git
+ln gitconfig ~/.gitconfig
+
+# Xorg
+ln xinitrc ~/.xinitrc
+ln Xresources ~/.Xresources
+
 
 # Input method.
 git clone git@github.com:/sheepduke/rime-wubi86 ~/projects/config/rime-wubi86
 ln -s ~/projects/config/rime-wubi86 ~/.config/ibus/rime
-cd ~/.config/ibus/rime ; rime_deployer --build . ; cd ~/
+rime_deployer --build ~/.config/ibus/rime
 
 # Emacs.
 git clone git@github.com:/sheepduke/emacs-settings ~/.emacs.d
 cp ~/.emacs.d/settings/.emacs ~/
-
-# Common Lisp.
-git clone git@github.com:/sheepduke/silver-brain ~/projects/lisp/silver-brain
