30480db76ae8ea1277b84839de08b74b13777ce0
YUE Daian <sheepduke@gmail.com>
2020-03-28 00:51:46 +0800

    feat: enhance bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-mkdir -p ~/projects/config/
-
 # Oh My ZSH
 echo "Setting up ZSH..."
 git clone git@github.com:/ohmyzsh/ohmyzsh ~/.oh-my-zsh
 ln -s home/zshrc ~/.zshrc
 ln -s home/tmux.conf ~/.tmux.conf
+cp home/.oh-my-zsh/custom/themes/my.zsh-theme ~/.oh-my-zsh/custom/themes/
 
 # Git
 cp home/gitconfig ~/.gitconfig
