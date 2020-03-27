12b07a8b78dc86a908c8af4200aa4efb6d42c8d9
YUE Daian <sheepduke@gmail.com>
2020-03-28 01:02:21 +0800

    fix: add missing zsh-syntax-highlighting

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@
 # Oh My ZSH
 echo "Setting up ZSH..."
 git clone git@github.com:/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+git clone git@github.com:/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 ln -s home/zshrc ~/.zshrc
 ln -s home/tmux.conf ~/.tmux.conf
 cp home/.oh-my-zsh/custom/themes/my.zsh-theme ~/.oh-my-zsh/custom/themes/
