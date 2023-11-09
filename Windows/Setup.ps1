70f6b8a00c6117d59303d5b2b26e543d398ea76c
Daian YUE 🐏 <daiyue@microsoft.com>
2023-11-09 11:54:09 +0800

    feat(windows): add setup.ps1

---
+++ Windows/Setup.ps1
@@ -0,0 +1,16 @@
+winget install 7zip.7zip
+winget install Git.Git
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.PowerShell
+winget install Microsoft.PowerToys
+winget install voidtools.Everything
+winget install GNU.Emacs
+winget install FSFhu.Hunspell
+winget install AutoHotkey.AutoHotkey
+winget install Mozilla.Firefox
+winget install Rime.Weasel
+
+mkdir ~/projects
+git clone https://github.com/sheepduke/rime-config ~/projects/rime-config
+git clone https://github.com/sheepduke/sane-config ~/projects/sane-config
+git clone https://github.com/sheepduke/emacs-config ~/.emacs.d
